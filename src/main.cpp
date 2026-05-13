b3c1dd7f4672d126683503af625920e442177977
Vladislav Isaychev <gregnov69@gmail.com>
2026-05-14 02:59:58 +0300

    added basic traverse logic; since the task never mentioned whether or not i am allowed to use external libraries i've decided to use libs for json, http, and cli interface :\

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,18 @@
+#include <algorithm>
 #include <chrono>
+#include <cstdlib>
 #include <ctime>
 #include <exception>
 #include <filesystem>
 #include <iostream>
+#include <map>
+#include <nlohmann/json.hpp>
 #include <ostream>
 #include <stdexcept>
 #include <string>
+#include <string_view>
+
+using json = nlohmann::json;
 
 namespace media_finder
 {
@@ -13,6 +20,60 @@ namespace media_finder
   {
     out << "usage: media_finder <directory_path> <timeout_in_seconds>\n";
   }
+
+  std::filesystem::path get_home_path()
+  {
+    const char* home = std::getenv("HOME");
+    return std::filesystem::path{ home };
+  }
+
+  class directory_entries_collector
+  {
+  public:
+    directory_entries_collector() : categorized_media_{} { clear(); }
+
+    void operator()(const std::filesystem::directory_entry& entry)
+    {
+      if (!entry.is_regular_file())
+      {
+        return;
+      }
+
+      try
+      {
+        categorized_media_[extension_type_mapping.at(
+                             entry.path().extension().c_str())]
+          .push_back(entry.path().string());
+      }
+      catch (...)
+      {}
+    }
+
+    void clear()
+    {
+      categorized_media_ = { { "audio", json::array() },
+                             { "video", json::array() },
+                             { "images", json::array() } };
+    }
+
+    std::string to_json() const { return categorized_media_.dump(); };
+
+  private:
+    json categorized_media_;
+
+    const inline static std::map< std::string_view, std::string_view >
+      extension_type_mapping = {
+        { ".wav", "audio" },   { ".aac", "audio" },   { ".wma", "audio" },
+        { ".flac", "audio" },  { ".ogg", "audio" },   { ".mp3", "audio" },
+        { ".alac", "audio" },  { ".aiff", "audio" },  { ".mp4", "video" },
+        { ".mov", "video" },   { ".mkv", "video" },   { ".webm", "video" },
+        { ".wmv", "video" },   { ".avi", "video" },   { ".jpeg", "images" },
+        { ".jpg", "images" },  { ".png", "images" },  { ".webp", "images" },
+        { ".gif", "images" },  { ".avif", "images" }, { ".svg", "images" },
+        { ".tiff", "images" }, { ".heif", "images" }, { ".heic", "images" },
+      };
+  };
+
 }
 int
 main(int argc, char** argv)
@@ -35,6 +96,15 @@ main(int argc, char** argv)
     std::chrono::seconds timeout(std::stoul(argv[2]));
 
     std::cout << directory_to_monitor << ' ' << timeout.count() << '\n';
+
+    media_finder::directory_entries_collector collector;
+
+    auto begin =
+      std::filesystem::recursive_directory_iterator{ directory_to_monitor };
+    auto end = std::filesystem::recursive_directory_iterator{};
+    std::for_each(begin, end, std::ref(collector));
+
+    std::cout << collector.to_json() << '\n';
   }
   catch (const std::exception& e)
   {
