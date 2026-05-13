1aeadc0b1dfcd4631ab7d18562a439de67133c52
Vladislav Isaychev <gregnov69@gmail.com>
2026-05-13 21:34:06 +0300

    added cl arguments handling && basic error handling; trying to think how cl interface will look

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,44 @@
+#include <chrono>
+#include <ctime>
+#include <exception>
+#include <filesystem>
 #include <iostream>
+#include <ostream>
+#include <stdexcept>
+#include <string>
 
-int main(int argc, char **argv) { std::cout << "ye\n"; }
+namespace media_finder
+{
+  void print_help_message(std::ostream& out)
+  {
+    out << "usage: media_finder <directory_path> <timeout_in_seconds>\n";
+  }
+}
+int
+main(int argc, char** argv)
+{
+  try
+  {
+    if (argc != 3)
+    {
+      media_finder::print_help_message(std::cerr);
+      throw std::runtime_error("wrong amount of arguments");
+    }
+
+    std::filesystem::path directory_to_monitor(argv[1]);
+
+    if (!std::filesystem::is_directory(directory_to_monitor))
+    {
+      throw std::runtime_error("provided path is not a directory");
+    }
+
+    std::chrono::seconds timeout(std::stoul(argv[2]));
+
+    std::cout << directory_to_monitor << ' ' << timeout.count() << '\n';
+  }
+  catch (const std::exception& e)
+  {
+    std::cerr << e.what() << '\n';
+    return -1;
+  }
+}
